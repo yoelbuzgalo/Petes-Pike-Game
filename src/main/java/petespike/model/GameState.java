efac0938cc0f8a482be71d0d73f6887941331589
Owen <owenlowry21@gmail.com>
2024-03-29 15:04:06 -0400

    exception and game state

---
+++ src/main/java/petespike/model/GameState.java
@@ -0,0 +1,8 @@
+package petespike.model;
+
+public enum GameState {
+    NEW, 
+    IN_PROGRESS,
+    NO_MOVES,
+    WON;
+}
---
+++ src/main/java/petespike/model/PetesPikeException.java
@@ -0,0 +1,8 @@
+package petespike.model;
+
+public class PetesPikeException extends Exception{
+
+    public PetesPikeException(String message){
+        super(message);
+    }
+}
