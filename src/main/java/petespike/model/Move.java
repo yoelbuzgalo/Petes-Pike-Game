6ad968e61d161a698beb547b77ca2e327334db03
Yoel Buzgalo <yoelbuzgalo@gmail.com>
2024-03-30 13:15:27 -0400

    Added Move class

---
+++ src/main/java/petespike/model/Move.java
@@ -0,0 +1,19 @@
+package petespike.model;
+
+public class Move {
+    private final Position position;
+    private final Direction direction;
+
+    public Move(Position position, Direction direction){
+        this.position = position;
+        this.direction = direction;
+    }
+
+    public Position getPosition() {
+        return this.position;
+    }
+
+    public Direction getDirection() {
+        return this.direction;
+    }
+}
