ab97fabee226526630d3ceef096f277ab4c834fb
Yoel Buzgalo <yoelbuzgalo@gmail.com>
2024-04-08 17:48:49 -0400

    JavaFX Added Status Updater

--- src/main/java/petespike/view/PetesPikeObserver.java
+++ src/main/java/petespike/view/PetesPikeObserver.java
@@ -1,5 +1,6 @@
 package petespike.view;
 
+import petespike.model.GameState;
 import petespike.model.Position;
 
 public interface PetesPikeObserver {
@@ -7,4 +8,5 @@ public interface PetesPikeObserver {
     public abstract void pieceMoved(Position from , Position to);
     public abstract void reset();
     public abstract void displayMessage(String message);
+    public abstract void updateStatus(GameState status);
 }
