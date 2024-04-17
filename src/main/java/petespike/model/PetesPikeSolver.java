53e61210a7a9f57b82c386a67915882c21b84359
Owen <owenlowry21@gmail.com>
2024-04-17 15:17:23 -0400

    solver update

--- src/main/java/petespike/model/PetesPikeSolver.java
+++ src/main/java/petespike/model/PetesPikeSolver.java
@@ -1,27 +1,41 @@
-//package petespike.model;
-//
-//import java.util.Collection;
-//
-//import backtracker.Configuration;
-//
-//public class PetesPikeSolver implements Configuration<PetesPike>{
-//
-//    @Override
-//    public Collection<PetesPike> getSuccessors() {
-//        // TODO Auto-generated method stub
-//        throw new UnsupportedOperationException("Unimplemented method 'getSuccessors'");
-//    }
-//
-//    @Override
-//    public boolean isValid() {
-//        // TODO Auto-generated method stub
-//        throw new UnsupportedOperationException("Unimplemented method 'isValid'");
-//    }
-//
-//    @Override
-//    public boolean isGoal() {
-//        // TODO Auto-generated method stub
-//        throw new UnsupportedOperationException("Unimplemented method 'isGoal'");
-//    }
-//
-//}
+package petespike.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import backtracker.Configuration;
+
+public class PetesPikeSolver implements Configuration<PetesPikeSolver>{
+    private List<Move> moves;
+    private PetesPike engine;
+
+    public PetesPikeSolver(PetesPike engine){
+        this.engine = new PetesPike(engine);
+        moves = new ArrayList<>();
+    }
+
+   @Override
+   public Collection<PetesPikeSolver> getSuccessors() {
+       Collection<PetesPikeSolver> collection = new Collection<PetesPikeSolver>();
+   }
+
+   @Override
+   public boolean isValid() {
+       try{
+        engine.makeMove(moves.get(-1));
+
+        return true;
+       }
+
+       catch(PetesPikeException e){
+            return false;
+       }
+   }
+
+   @Override
+   public boolean isGoal() {
+       return engine.getState() == GameState.WON;
+   }
+
+}
